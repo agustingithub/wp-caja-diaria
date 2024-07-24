3999980598ebf4a3e66a847b0745afc20c4b4033
agustingithub <41640843+agustingithub@users.noreply.github.com>
2024-07-24 12:22:02 -0300

    Delete includes directory

--- includes/wpcajadiaria-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Función auxiliar para sanitizar la entrada
-function wpcd_sanitize_input($input) {
-    if (is_numeric($input)) {
-        return floatval($input); // Para entradas numéricas
-    } else {
-        return sanitize_text_field($input); // Para entradas de texto
-    }
-}
